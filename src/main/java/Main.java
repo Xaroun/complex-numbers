93fa1c9345dab6eb814da30d8cf0310bf8c93d0a
Xaroun <mateusz1003@gmail.com>
2018-11-24 15:26:17 +0100

    main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,24 @@ public class Main {
         ComplexNumber firstNumber = new ComplexNumber(1, 2);
         ComplexNumber secondNumber = new ComplexNumber(3, -4);
 
+        //Dodawanie
+        System.out.println("Dodawanie: " + firstNumber + " oraz " + secondNumber + " = " + firstNumber.add(secondNumber));
+
+        //Odejmowanie
+        System.out.println("Odejmowanie: " + firstNumber + " oraz " + secondNumber + " = " + firstNumber.subtract(secondNumber));
+
+        //Mnożenie
+        System.out.println("Mnożenie: " + firstNumber + " oraz " + secondNumber + " = " + firstNumber.multiply(secondNumber));
+
+        //Dzielenie
+        System.out.println("Dzielenie: " + firstNumber + " oraz " + secondNumber + " = " + firstNumber.divide(secondNumber));
+
+        //Sprzężenie
+        System.out.println("Sprzężenie: " + firstNumber + " = " + firstNumber.conjugate());
+
+        //Moduł
+        System.out.println("Moduł: " + firstNumber + " = " + firstNumber.module());
+
         List<ComplexNumber> w = new ArrayList<>();
         w.add(firstNumber);
         w.add(secondNumber);
@@ -22,7 +40,17 @@ public class Main {
         v.add(thirdNumber);
         v.add(fourthNumber);
 
-        ComplexNumber multipliedVectors = Vector.multiply(w, v);
-        System.out.println(multipliedVectors);
+        //Dodawanie wielomianów
+        System.out.println("Dodawanie wielomianów: " + v + " oraz " + w + " = " + Vector.addition(v, w));
+
+        //Odejmowanie wielomianów
+        System.out.println("Odejmowanie wielomianów: " + v + " oraz " + w + " = " + Vector.subtraction(v, w));
+
+        //Mnożenie skalarne wielomianów
+        System.out.println("Mnożenie skalarne wielomianu: " + v + " oraz " + secondNumber + " = " + Vector.scalarMultiply(v, secondNumber));
+
+        //Mnożenie wielomianów
+        System.out.println("Mnożenie wielomianu: " + v + " oraz " + w + " = " + Vector.multiply(v, w));
+
     }
 }
